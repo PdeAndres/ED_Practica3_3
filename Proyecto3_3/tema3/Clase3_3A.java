4de40d9cd5d7c58f9e53a3bdd9ae888c97633dc5
pablo <pablo@DESKTOP-U77RNOT>
2023-11-29 12:28:17 +0100

    Cambiar el contenido del syso por "Canela en Rama" en la rama CanelaEnRama

--- Proyecto3_3/tema3/Clase3_3A.java
+++ Proyecto3_3/tema3/Clase3_3A.java
@@ -3,7 +3,7 @@ package tema3;
 public class Clase3_3A {
 
 	public static void main(String[] args) {
-		System.out.println("Clase de prueba para la práctica");
+		System.out.println("Canela En Rama");
 	}
 
 }
